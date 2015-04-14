595975915fd7690b0387a88914b6b4d02ea34f79
NicoSxt <123sxtnicole@gmail.com>
2015-04-14 19:23:22 -0400

    added final proposal & week10 hw

--- week10/js/main.js
+++ week10/js/main.js
@@ -1,44 +1,91 @@
-var yo = $('.yo');
+	var Weezer = {
+		Rivers: {
+		firstName: 'Rivers',
+		lastName: 'Cuomo',
+		ocupation: 'vocal_and_guitar',
+		//store objects within objects!
+		look: {
+		glasses: 'true',
+		//end the last propertie without a comma
+		gender: 'male',
+		randomInterest: 'soccer'
+		}
+		},
 
+		Patrick: {
+		firstName: 'Patrick',
+		lastName: 'Wilson',
+		ocupation: 'drummer',
+		//store objects within objects!
+		look: {
+		glasses: 'true',
+		//end the last propertie without a comma
+		gender: 'male',
+		randomInterest: 'EP'
+		}
+		},
 
-	 worker1: {
-		firstName: 'King',
-		lastName: 'Kong',
-		favorateFood: 'bananas',
-		favorateColor:{
-			forFood: 'yellow',
-			forShower: 'pink',
-			forChill: 'red',
-			forBed: 'orange',
-			forPooping: 'purple',
-			forWalking: 'blue'
+		Jason: {
+		firstName: 'Jason',
+		lastName: 'Cropper',
+		ocupation: 'guitar',
+		//store objects within objects!
+		look:{
+		glasses: 'either',
+		//end the last propertie without a comma
+		gender: 'male',
+		randomInterest: 'nothing'
+		}
+		},
+
+		Matt: {
+		firstName: 'Matt',
+		lastName: 'Sharp',
+		ocupation: 'bass',
+			//store objects within objects!
+			look: {
+			glasses: 'either',
+			//end the last propertie without a comma
+			gender: 'male',
+			randomInterest: 'falsetto'
+			}
 		}
 	}
 
-	 worker2: {
-		firstName: 'Koing',
-		lastName: 'Kang',
-		favorateFood: 'apple',
-		favorateColor:{
-			forFood: 'blue',
-			forShower: 'red',
-			forChill: 'yellow',
-			forBed: 'pink',
-			forPooping: 'black',
-			forWalking: 'brown'
-		}
+	for(var member in Weezer){
+	var weezerInfo = Weezer[member];
+
+
+	//div
+	var container = document.createElement('div');
+	//title
+	var title = document.createElement('h2');
+
+	var occupation = document.createElement('div');
+	var glasses = document.createElement('div');
+	var gender = document.createElement('div');
+	var randomInterest = document.createElement('div');
+	//fill title with contact name
+	title.innerHTML = 'this weezer member is : ' + weezerInfo.firstName +' '+ weezerInfo.lastName;
+	occupation.innerHTML = 'he plays ' + weezerInfo.ocupation + ' in the band.';
+	glasses.innerHTML = 'dose he wear glasses? ' + weezerInfo.look.glasses ;
+	gender.innerHTML = 'and what is his gender? ' + weezerInfo.look.gender ;
+	randomInterest.innerHTML = 'Here is a random interest of him: ' + weezerInfo.look.randomInterest ;
+	//place the html element wihtin the DOM
+	//add titel to container
+	container.appendChild(title);
+	container.appendChild(occupation);
+	container.appendChild(glasses);
+	container.appendChild(gender);
+	container.appendChild(randomInterest);
+	//add to webpage
+	document.body.appendChild(container);
+
+	console.log(weezerInfo);
+	//list everysingle element within the objects(contacts) inside contactList
+	console.log(Weezer[member]);
 	}
-	 worker3: {
-		firstName: 'Keng',
-		lastName: 'Kiong',
-		favorateFood: 'whatever',
-		favorateColor:{
-			forFood: 'wahtever',
-			forShower: 'wotever',
-			forChill: 'wtever',
-			forBed: 'otever',
-			forPooping: 'witaver',
-			forWalking: 'whetever'
-		}
-	}
-}
+	var matt = Weezer.matt;
+	//access data within an object using dot notation
+	console.log(Weezer.Matt.firstName);
+	// console.log(matt['ocupation']);
